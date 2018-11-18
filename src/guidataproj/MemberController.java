35f0afc08bd642166d7b337c6b0886703daa29ea
tamartin9013 <42901247+tamartin9013@users.noreply.github.com>
2018-11-17 22:46:10 -0500

    Add files via upload

---
+++ src/guidataproj/MemberController.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guidataproj;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.control.TextField;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Gamer
+ */
+public class MemberController {
+    
+    Connection connection = null;
+    String dbName="Members";
+    String userName="root";
+    String password="";
+    
+    public Connection connectDB(){
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");                    
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/"+dbName,userName,password);
+            JOptionPane.showMessageDialog(null, "Connected to database");
+            return connection;
+        } 
+        catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+
+
+        
+    }
+
+    
+    
+}
